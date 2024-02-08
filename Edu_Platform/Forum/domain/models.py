693627bb029fac358e4fa085d7d4483fac6ae8cc
Sviatoslav Kostyshyn <83891510+kossvat@users.noreply.github.com>
2024-02-08 18:54:19 -0500

    Create Models and make migrations. (#10)
    * Create Models and make migrations.
    
    * make migrations.

--- Edu_Platform/Forum/domain/models.py
+++ Edu_Platform/Forum/domain/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class ForumThread(models.Model):
+    course_id = models.IntegerField()
+    title = models.CharField(max_length=200)
+    content = models.TextField(null=True, blank=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    
+    def __str__(self):
+        return f'{self.title} [Course ID: {self.course_id}]'
+
+
+class ThreadReply(models.Model):
+    thread = models.ForeignKey(
+        ForumThread,
+        on_delete=models.CASCADE,
+        related_name='replies'
+    )
+    user_id = models.IntegerField(null=True, blank=True)
+    content = models.TextField(null=True, blank=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    
+    def __str__(self):
+        return f'Reply by User {self.user_id} on "{self.thread.title}"'
